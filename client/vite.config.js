f71a8c759641d86fb65df48ad72b9648dc22b504
patch994r <anouarmohammed994@gmaile.com>
2026-05-21 23:47:46 +0100

    Préparation pour déploiement

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,6 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+export default defineConfig({
+  plugins: [react()],
+  base: '/neura-x-agency/', // Important : mets exactement le nom de ton repo GitHub
+})
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
